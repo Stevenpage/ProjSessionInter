6227556ca6fa4984fdbbc7b00febfc6e21c270ef
Stevenpage <1341500@LLBD0604INFO14.laboratoire.collegeem.qc.ca>
2015-12-01 17:14:32 -0500

    Added DaysRepository Skeleton

--- ProjetFinalEchApp/ProjetEchAppClient/js/daysController.js
+++ ProjetFinalEchApp/ProjetEchAppClient/js/daysController.js
@@ -3,22 +3,60 @@ var daysController = angular.module('daysController', []);
 daysController.controller('DaysController', function DaysController($scope, $http, $rootScope) {
 
     $scope.Voyage;
-    
+
+    $scope.GetForVoyage = function () {
+        $http({
+            method: 'GET',
+            header: { Authorization: 'Bearer ' + localStorage.getItem($scope.TOKEN_KEY) },
+            TripId: $scope.Voyage.Id,
+            url: "http://localhost:53407/api/DaysByVoyage"
+        }).success(function (data) {
+            console.log(data)
+            $scope.Memos = data;
+        });
+    }
 
     $scope.AddDay = function () {
         $http({
             method: 'POST',
-            url: "http://localhost:XXXX/api/Days",
+            url: "http://localhost:53407/api/Days",
             header: { Authorization: 'Bearer ' + localStorage.getItem($scope.TOKEN_KEY) },
             data:
             {
-                texte: $scope.AddTripId,
-                titre: $scope.titreadd,
-                date: $scope.dateadd,
-                //UtilisateurId: localStorage.getItem($scope.TOKEN_KEY).id
-                UtilisateurId: "I don't care about that!"
+                TripId: $scope.Voyage.Id,
+                TranportType: $scope.AddTransportType,
+                StartDate: $scope.AddStartDate,
+                EndDate: $scope.AddEndDate,
+                BudgetLimit: $scope.AddBudgetLimit
             }
 
+        }).success(function (data) {
+            console.log(data)
+        });
+    }
+
+    $scope.DeleteLastDay = function () {
+        $http({
+            method: 'GET',
+            header: { Authorization: 'Bearer ' + localStorage.getItem($scope.TOKEN_KEY) },
+            url: "http://localhost:53407/api/DaysByVoyage"
+        }).success(function (data) {
+            console.log(data[data.length - 1].MemoId)
+            $http({
+                method: 'DELETE',
+                url: "http://localhost:53407/api/DaysByVoyage" + data[data.length - 1].MemoId
+            }).success(function (data) {
+                console.log(data)
+            });
+            console.log(data)
+        });
+    }
+
+    $scope.DeleteById = function () {
+        $http({
+            method: 'DELETE',
+            header: { Authorization: 'Bearer ' + localStorage.getItem($scope.TOKEN_KEY) },
+            url: "http://localhost:5813/api/Memos/" + $scope.IdDelete
         }).success(function (data) {
             console.log(data)
         });
