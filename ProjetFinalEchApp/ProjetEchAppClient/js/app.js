cf7a6f3d2f6f5509d2caac322a71fdb73bcf2c17
Godfathcr <1350529@LLBD0604INFO15.laboratoire.collegeem.qc.ca>
2015-12-01 17:24:35 -0500

    Template Voyage.html

--- ProjetFinalEchApp/ProjetEchAppClient/js/app.js
+++ ProjetFinalEchApp/ProjetEchAppClient/js/app.js
@@ -1,1 +1,8 @@
-var memorizerInfinie = angular.module('memorizerInfinie', ['loginController','tripsController','daysController']);
+var memorizerInfinie = angular.module('memorizerInfinie', ['loginController', 'tripsController', 'daysController']);
+
+memorizerInfinie.run(['$rootScope', function ($rootScope) {
+
+    $rootScope.templates = [{ name: 'TemplateVoyage', url: 'templates/Voyage.html' }];
+    $rootScope.voyageTemplateUrl = $rootScope.templates[0].url;
+
+}])
--- ProjetFinalEchApp/ProjetEchAppClient/js/tripsController.js
+++ ProjetFinalEchApp/ProjetEchAppClient/js/tripsController.js
@@ -1,4 +1,7 @@
 var tripsController = angular.module('tripsController', []);
 
 tripsController.controller('TripsController', function TripsController($scope, $http, $rootScope) {
+
+
+
 });
